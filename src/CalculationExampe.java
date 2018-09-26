1417763526be7f286c82fa127d7966f22086f7fb
Trinh Hau <congtuqwe@gmail.com>
2018-09-26 09:29:05 +0700

    Calculator Exception

---
+++ src/CalculationExampe.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CalculationExampe {
+
+    private void caculate(int x, int y) {
+        try {
+            int a = x + y;
+            int b = x - y;
+            int c = x * y;
+            int d = x / y;
+            System.out.println("Tổng x + y = " + a);
+            System.out.println("Hiệu x - y = " + b);
+            System.out.println("Tích x * y = " + c);
+            System.out.println("Thương x / y = " + d);
+        } catch (Exception e) {
+            System.out.println("Xay ra ngoai le: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hãy nhập x: ");
+        int x = scanner.nextInt();
+        System.out.println("Hãy nhập y: ");
+        int y = scanner.nextInt();
+        CalculationExampe calc = new CalculationExampe();
+        calc.caculate(x, y);
+    }
+}
